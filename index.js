d9350c0b03211a94f2b202a405fe53d7f2013d4f
Shanuka <shanukathavanesan@gmail.com>
2024-01-30 22:45:48 +0530

    commited

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello, world!');
+  });
+
+  app.listen(3000, () => {
+    console.log('Server started on port 3000');
+  });
+  app.post('/resources', (req, res) => {
+    const { name } = req.body;
+    // Create new resource with provided name
+    res.status(201).json({ message: `Created resource with name ${name}` });
+  });
